88983926b2fbc6799b99bc60ec6327aff6470b32
Ha Nguyen <nguyenthingocha2@gmail.com>
2020-03-06 12:00:20 -0500

    install JSON package

--- note-taker/README.md
+++
@@ -1,2 +0,0 @@
-# note-taker
-An application that can be used to write, save, and delete notes.
--- server.js
+++ server.js
@@ -1,27 +1,48 @@
 const express = require("express");
+const fs
 var path = require("path");
-
-var app = express();
+//const {uuid} = require("uuidv4")
+const app = express();
 var PORT = process.env.PORT || 3000;
+
+const db = require("./db/db.json");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const db = require("./db/db/json");
 
-app.get("/", function(req, res){
-   
-    res.sendFile(path.join(__dirname, "public/index.html"));
+  
+  // app.get("/add", function(req, res) {
+  //   res.sendFile(path.join(__dirname, "public/notes.html"));
+  // });
+//get the database:
+
+  app.get("/notes", function(req, res) {
+    var notes = req.params.db;
+    console.log(db);
+  })
+
+  //get New note:
+  let newNote = req.body;
+
+  // save new note: 
+
+  //delete new note:
+
+
+  
+  app.post("/api/db", function(req, res) {
+    var newdb = req.body;
+    console.log(db);
+});
+
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
 
-  //install npm i
 
-  app.get("/notes", function(req,res) {
 
-  });
-  // add res.params.id after the app.delete
-  //use splice to remove
